9237b3875e6f6e73637a8c54a06a9bd4453e74c6
Jason Guo <jasongu@ad.unc.edu>
2021-03-01 00:44:57 -0500

    k

--- src/a2/LinkedList.java
+++ src/a2/LinkedList.java
@@ -22,7 +22,7 @@ public class LinkedList {
         Node current = head, prev = null;
         int j = 0;
         if(i == 0) {
-            head = head.getNext();
+            current = current.getNext();
         }
         while (j < i) {
             prev = current;
